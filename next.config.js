2b992cc5a6848ecc56c0cd754f964a181d145118
mayankSonkar2@14 <55187056+Mayank1-cpu@users.noreply.github.com>
2023-07-08 22:00:34 +0530

    add PWA functionality

--- next.config.js
+++ next.config.js
@@ -1,8 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
+
+const withPWA = require("next-pwa")({
+  dest: "public",
+  register: true,
+  skipWaiting: true,
+  disable: process.env.NODE_ENV === "development",
+});
+
+module.exports = withPWA({
   images: {
     domains: ["media.giphy.com"],
   },
-};
-
-module.exports = nextConfig;
+});
